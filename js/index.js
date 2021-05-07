9214689641304353147c2da8dc8fba991e75a092
Sergio Gomez <laragsergio1803@gmail.com>
2021-05-06 23:54:54 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function carga() {
     var archivoTxt = new XMLHttpRequest();
-    var fileRuta = "https://github.com/SagLara/CV-Personal/blob/master/utils/back-obj.txt";
+    var fileRuta = "../utils/back-obj.txt";
 
     archivoTxt.open("GET", fileRuta, false);
     archivoTxt.send(null);

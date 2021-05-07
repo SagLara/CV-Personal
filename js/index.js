95800938c9b3b9558c2ac0654d6206c271021320
Sergio Gomez <laragsergio1803@gmail.com>
2021-05-06 23:45:52 -0500

    correcion error carga

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-var archivoTxt = new XMLHttpRequest();
-var fileRuta = "../utils/back-obj.txt";
+function carga() {
+    var archivoTxt = new XMLHttpRequest();
+    var fileRuta = "../utils/back-obj.txt";
 
-archivoTxt.open("GET", fileRuta, false);
-archivoTxt.send(null);
+    archivoTxt.open("GET", fileRuta, false);
+    archivoTxt.send(null);
 
-var txt = archivoTxt.responseText;
+    var txt = archivoTxt.responseText;
 
-var objetivo = document.getElementById('objetivo');
-objetivo.innerHTML = txt;
+    var objetivo = document.getElementById('objetivo');
+    objetivo.innerHTML = txt;
+}

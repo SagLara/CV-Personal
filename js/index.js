cdebd593dc22c8e0e8671efd5f934226d779ea09
Sergio Gomez <laragsergio1803@gmail.com>
2021-05-07 00:11:12 -0500

    add index js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var archivoTxt = new XMLHttpRequest();
+var fileRuta = "../utils/back-obj.txt";
+
+archivoTxt.open("GET", fileRuta, false);
+archivoTxt.send(null);
+
+var txt = archivoTxt.responseText;
+
+var objetivo = document.getElementById('objetivo');
+objetivo.innerHTML = txt;
